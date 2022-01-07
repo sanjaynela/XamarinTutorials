c4010cd6179c8525601f465c8d7c6803c9c63763
Sanjay Nelagadde <sanjay@sonavilabs.com>
2022-01-07 10:59:05 -0800

    Save tutorials to git

---
+++ MyFirstMobileApp/MyFirstMobileApp/MyFirstMobileApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MyFirstMobileApp.ViewModels;
+using MyFirstMobileApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MyFirstMobileApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ MyFirstMobileApp/MyFirstMobileApp/MyFirstMobileApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MyFirstMobileApp.Models;
+using MyFirstMobileApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyFirstMobileApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
